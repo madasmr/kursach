1a4ac332b2cb11b7d350858d5b7ebeb3f54fd0af
madasmr <167167835+madasmr@users.noreply.github.com>
2025-01-16 22:28:28 +0700

    Add files via upload

---
+++ Store.Contractors.YandexKassa/Areas/YandexKassa/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Contractors.YandexKassa.Areas.YandexKassa.Models;
+namespace Store.Contractors.YandexKassa.Areas.YandexKassa.Controllers
+{
+	[Area("YandexKassa")]
+	public class HomeController : Controller
+	{
+		public IActionResult Index(int orderId, string returnUri)
+		{
+			var model = new ExampleModel
+			{
+				OrderId = orderId,
+				ReturnUri = returnUri,
+			};
+			return View(model);
+		}
+		public IActionResult Callback(int orderId, string returnUri)
+		{
+			var model = new ExampleModel
+			{
+				OrderId = orderId,
+				ReturnUri = returnUri,
+			};
+			return View(model);
+		}
+	}
+}
