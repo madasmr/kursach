223592f5d6b58263c3ec05928fb8abe25823941f
madasmr <167167835+madasmr@users.noreply.github.com>
2025-01-16 22:52:15 +0700

    Add files via upload

---
+++ Store.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wise_nut;
+using wise_nut.Web.App;
+
+namespace Store.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+		private readonly ProductService productService;
+		public ProductController(ProductService productService)
+		{
+			this.productService = productService;
+		}
+        public IActionResult Index(int id)
+        {
+			var model = productService.GetById(id);
+
+			return View(model);
+        }
+    }
+}
---
+++ Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wise_nut;
+using wise_nut.Web.App;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ProductService productService;
+
+        public SearchController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+        public IActionResult Index(string query)
+        {
+            var products = productService.GetAllByQuery(query);
+            return View("Index", products);
+        }
+    }
+}
