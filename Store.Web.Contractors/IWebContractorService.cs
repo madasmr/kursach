309474d3c37d31a0d7f58acafcd97c1bba09aefb
madasmr <167167835+madasmr@users.noreply.github.com>
2025-01-16 22:23:26 +0700

    Add files via upload

---
+++ Store.Web.Contractors/IWebContractorService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Store.Web.Contractors
+{
+	public interface IWebContractorService
+	{
+		string Name { get; }
+		Uri StartSession(IReadOnlyDictionary<string, string> parameters, Uri returnUri);
+	}
+}
